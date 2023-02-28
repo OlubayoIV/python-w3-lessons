a0e4328879c377ff3d0dfd620d7549a8425a730a
Ayo <olubayo.ayobami21@gmail.com>
2023-02-28 03:04:43 +0000

    new commit

--- index.py
+++ index.py
@@ -265,3 +265,32 @@ for x in range(1, 23, 2):
 def my_function():
     print('new pyhton')
     my_function()
+
+    #how to find files in python
+name = input('Enter file name:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
+
+    #instagram in python
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else :
+        counts[name] = counts[name] + 1
+print(counts)
